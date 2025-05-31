591bdf89661b2ee0629b9b5ba5163c04a450a46a
Developer <marcioserrasales2014@gmail.com>
2025-05-31 19:53:05 -0300

    Initial commit

---
+++ src/main/java/com/marciosales/santander_dev_week/SantanderDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package com.marciosales.santander_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marciosales/santander_dev_week/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marciosales.santander_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
